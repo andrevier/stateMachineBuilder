1da45492a1f4acc4b03a9d19d30d5e2e82970965
andrevier <rangel_andre@outlook.com>
2023-06-22 10:45:33 -0300

    issue #2: Update objectives in log.txt.
    Change the switchedOffEvents type (cell array).

---
+++ State.m
@@ -0,0 +1,26 @@
+classdef State 
+    properties
+        number
+        name
+        activeEvents
+    end
+    methods
+        function obj = State(number, name, activeEvents)
+            obj.number = number;
+            obj.name = name;
+            obj.activeEvents = activeEvents;
+        end
+
+        function activeEvents = getActiveEvents(obj)
+            activeEvents = obj.activeEvents;
+        end
+
+        function name = getName(obj)
+            name = obj.name;
+        end
+
+        function number = getNumber(obj)
+            number = obj.number;
+        end
+    end
+end

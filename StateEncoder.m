6e11bd421ac3043bcc1e75216a41b8e42c050acc
Andre Rangel <andrerangel@ad.coe.ufrj.br>
2023-06-25 15:46:58 -0300

    issue #3: Create the test to encode and decode simple array of State objects.

--- StateEncoder.m
+++ StateEncoder.m
@@ -1,31 +1,28 @@
 % Encode the data from the States class to pass it as a parameter in
 % Simulink's Matlab functions block.
-classdef StatesEncoder
-    properties
-    end
-    methods
-        function encodedStatesArray = encode(statesArray, activeEvents)
+classdef StateEncoder
+    
+    methods(Static)
+        function encodedStatesArray = encode(statesArray)
             % Encode the states array as a double matrix where:
             % - 1º column as the state number;
             % - 2º to the end columns is the active events indicated by the
             % position of the events and the 1 for active and 0, for
             % inactive.           
             numLines = numel(statesArray);
-            numColumns = numel(activeEvents);
+            numColumns = numel(statesArray(1).activeEvents) + 1;
             encodedStatesArray = zeros(numLines, numColumns);
 
             for i = 1:numLines
-                stateNumber = statesArray(i).getNumber();
-                stateActiveEvents = statesArray(i).getActiveEvents();
-                encodedStatesArray(i, 1) = stateNumber;
-                encodedStatesArray(i, 2:end) = stateActiveEvents;
+                encodedStatesArray(i, 1) = statesArray(i).number;
+                encodedStatesArray(i, 2:end) = statesArray(i).activeEvents;
             end            
         end
 
         function statesArray = decode(encodedStatesArray)
             % Decode states array from the matrix format.
             numOfStates = size(encodedStatesArray, 1);
-            statesArray = States.empty(numOfStates, 0);
+            statesArray = State.empty(numOfStates, 0);
 
             for i = 1:numOfStates
                 stateNumber = encodedStatesArray(i, 1);
---
+++ TestStateEncoder.m
@@ -0,0 +1,32 @@
+classdef TestStateEncoder < matlab.unittest.TestCase
+    methods(Test)
+        function testEncodeStatesArray(testCase)
+            % Given the following list of states.
+            s0 = State(0, 's0', [1, 1, 0, 0]);
+            s1 = State(1, 's1', [1, 0, 0, 0]);
+            s2 = State(2, 's2', [1, 1, 1, 0]);
+            s = [s0, s1, s2];
+            actualEncodedArray = StateEncoder.encode(s);
+            
+            % Asserts that
+            expected = [0, 1, 1, 0, 0; 1, 1, 0, 0, 0; 2, 1, 1, 1, 0];
+
+            testCase.verifyEqual(actualEncodedArray, expected);           
+        end
+
+        function testDecodeintoStatesArray(testCase)
+            % When
+            encodedArray = [0, 1, 1, 0, 0; 1, 1, 0, 0, 0; 2, 1, 1, 1, 0];
+            actualDecodedArray = StateEncoder.decode(encodedArray);
+            
+            % Asserts that
+            s0 = State(0, 's0', [1, 1, 0, 0]);
+            s1 = State(1, 's1', [1, 0, 0, 0]);
+            s2 = State(2, 's2', [1, 1, 1, 0]);            
+
+            expectedStatesArray = [s0, s1, s2];
+            
+            testCase.verifyEqual(actualDecodedArray, expectedStatesArray);            
+        end
+    end
+end
